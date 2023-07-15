ca8de1e3d7a22ea64d286e3e1ca685b6eb6224cc
Washington Kimani <kimaniwashington39@gmail.com>
2023-07-15 15:12:48 +0300

    some fixes

--- app.js
+++ app.js
@@ -28,10 +28,34 @@ app.get('/', (req, res) => {
     res.send(`Hello Amigo`)
 })
 
-app.get('/api/products', (req, res) => {
-    const products = collection.find();
-    res.send(products);
-})
+// app.get('/api/products', (req, res) => {
+//     const products = collection.find();
+//     res.send(products);
+// })
+app.get('/api/products', async (req, res) => {
+    // Your existing code here
+    const products = await collection.find();
+
+    try {
+        const jsonString = JSON.stringify(products, (key, value) => {
+            // Exclude circular references from the serialization
+            if (typeof value === 'object' && value !== null) {
+                if (key === 'client' || key === 's' || key === 'sessionPool') {
+                    return '[Circular]';
+                }
+            }
+            return value;
+        });
+
+        // Send the jsonString as the response
+        res.send(jsonString);
+    } catch (error) {
+        console.error('Error converting object to JSON:', error);
+        // Handle the error and send an appropriate response
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 
 app.get('/api/product/:id', (req, res) => {
     const product = collection.findOne({
